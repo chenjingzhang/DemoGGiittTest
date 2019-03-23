c7c3728f961835922a0663b5fc0c79414d952f16
zhangxiaoyan <hellen@dxt.cn>
2019-03-23 14:13:13 +0800

    gitmacforfirst

---
+++ app/src/main/java/rdemo/ggiitt/com/demoggiitt/MainActivity.java
@@ -0,0 +1,14 @@
+package rdemo.ggiitt.com.demoggiitt;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        System.out.print("fskdfs");
+    }
+}
